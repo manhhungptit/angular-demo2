7229a9120fb1dcc4f47a0643b7376ca502f94f24
mhung <mhungcnptit@gmail.com>
2020-03-09 16:01:15 +0700

    update issue change language on url

--- public/app.js
+++ public/app.js
@@ -3,13 +3,19 @@ const app = angular.module('myApp', ['ngRoute', 'pascalprecht.translate']);
 const DEFAULT_LANG = ['de', 'en'];
 
 const translationEN = {
+    USER: 'User',
+    CAR: 'Car',
     TITLE_USER: 'This is user page',
-    TITLE_CAR: 'This is car page'
+    TITLE_CAR: 'This is car page',
+    SELECT_LANG: 'Select language'
 }
 
 const translationDE = {
+    USER: 'Benutzer',
+    CAR: 'Auto',
     TITLE_USER: 'Dies ist eine Benutzerseite',
-    TITLE_CAR: 'Dies ist Autoseite'
+    TITLE_CAR: 'Dies ist Autoseite',
+    SELECT_LANG: 'Sprache auswählen'
 }
 
 app.config(['$routeProvider', '$locationProvider', '$translateProvider',
@@ -32,8 +38,8 @@ app.config(['$routeProvider', '$locationProvider', '$translateProvider',
         $translateProvider.preferredLanguage('en');
     }])
 
-app.controller('mainController', ['$scope', '$location', '$translate',
-    function ($scope, $location, $translate) {
+app.controller('mainController', ['$scope', '$location', '$translate', '$routeParams', '$location',
+    function ($scope, $location, $translate, $routeParams, $location) {
         let self = this;
 
         if (redirectUrl && redirectUrl != '/') {
@@ -45,9 +51,17 @@ app.controller('mainController', ['$scope', '$location', '$translate',
             {"name": "Germany", "code": "de"}
         ];
 
+        $scope.selectedLanguage = 'en';
+
         self.changeLanguage = function (language) {
             if (DEFAULT_LANG.indexOf(language) >= 0) {
-                $translate.use(language)
+                // $translate.use(language)
+                $scope.selectedLanguage = language;
+                if ($routeParams.lang) {
+                    let url = $location.url();
+                    url = url.replace($routeParams.lang, language);
+                    $location.path(url);
+                }
             }
         }
 
